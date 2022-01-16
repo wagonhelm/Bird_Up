dce4733ff55eb81ad74ae800e7628ba0b8e2976c
wagonhelm <jkf@ualberta.ca>
2022-01-16 05:15:23 -0700

    yolo yolo yolo

--- workspace/catkin_ws/src/vision/src/camera.py
+++ workspace/catkin_ws/src/vision/src/camera.py
@@ -7,47 +7,125 @@ roslib.load_manifest('vision')
 import sys
 import rospy
 import cv2
+import numpy as np
+import time
 from std_msgs.msg import String
 from sensor_msgs.msg import Image
 from cv_bridge import CvBridge, CvBridgeError
 
+CONFIDENCE = 0.5
+SCORE_THRESHOLD = 0.5
+IOU_THRESHOLD = 0.5
+config_path = "/home/jetson/Bird_Up/workspace/catkin_ws/src/vision/src/cfg/yolov3.cfg"
+weights_path = "/home/jetson/Bird_Up/workspace/catkin_ws/src/vision/src/weights/yolov3.weights"
+font_scale = 1
+thickness = 1
+LABELS = open("/home/jetson/Bird_Up/workspace/catkin_ws/src/vision/src/data/coco.names").read().strip().split("\n")
+COLORS = np.random.randint(0, 255, size=(len(LABELS), 3), dtype="uint8")
+
+net = cv2.dnn.readNetFromDarknet(config_path, weights_path)
+
+ln = net.getLayerNames()
+ln = [ln[i[0] - 1] for i in net.getUnconnectedOutLayers()]
+
 class image_converter:
 
-  def __init__(self):
-    self.image_pub = rospy.Publisher("/raccoon_detector",Image)
+    def __init__(self):
+        self.image_pub = rospy.Publisher("/raccoon_detector",Image, queue_size=1)
 
-    self.bridge = CvBridge()
-    self.image_sub = rospy.Subscriber("/jetbot_camera/raw",Image,self.callback)
+        self.bridge = CvBridge()
+        self.image_sub = rospy.Subscriber("/jetbot_camera/raw",Image,self.callback)
 
-  def callback(self,data):
-    try:
-	  # This is the image you want to work with
-      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
-    except CvBridgeError as e:
-      print(e)
+    def callback(self,data):
+        try:
+            # This is the image you want to work with
+            image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
 
-    (rows,cols,channels) = cv_image.shape
-    if cols > 60 and rows > 60 :
-      cv2.circle(cv_image, (50,50), 10, 255)
+            # Put your object detection code here Akrash
+        h, w = image.shape[:2]
+        blob = cv2.dnn.blobFromImage(image, 1/255.0, (416, 416), swapRB=True, crop=False)
+        net.setInput(blob)
 
-    #cv2.imshow("Image window", cv_image)
-    #cv2.waitKey(3)
+        layer_outputs = net.forward(ln)
 
-	# Put your object detection code here Akrash
+        boxes, confidences, class_ids = [], [], []
 
-    try:
-      self.image_pub.publish(self.bridge.cv2_to_imgmsg(cv_image, "bgr8"))
-    except CvBridgeError as e:
-      print(e)
+        # loop over each of the layer outputs
+        for output in layer_outputs:
+            # loop over each of the object detections
+            for detection in output:
+                # extract the class id (label) and confidence (as a probability) of
+                # the current object detection
+                scores = detection[5:]
+                class_id = np.argmax(scores)
+                confidence = scores[class_id]
+                # discard weak predictions by ensuring the detected
+                # probability is greater than the minimum probability
+                if confidence > CONFIDENCE:
+                    # scale the bounding box coordinates back relative to the
+                    # size of the image, keeping in mind that YOLO actually
+                    # returns the center (x, y)-coordinates of the bounding
+                    # box followed by the boxes' width and height
+                    box = detection[:4] * np.array([w, h, w, h])
+                    (centerX, centerY, width, height) = box.astype("int")
+
+                    # use the center (x, y)-coordinates to derive the top and
+                    # and left corner of the bounding box
+                    x = int(centerX - (width / 2))
+                    y = int(centerY - (height / 2))
+
+                    # update our list of bounding box coordinates, confidences,
+                    # and class IDs
+                    boxes.append([x, y, int(width), int(height)])
+                    confidences.append(float(confidence))
+                    class_ids.append(class_id)
+
+        # perform the non maximum suppression given the scores defined before
+        idxs = cv2.dnn.NMSBoxes(boxes, confidences, SCORE_THRESHOLD, IOU_THRESHOLD)
+
+        font_scale = 1
+        thickness = 1
+
+        # ensure at least one detection exists
+        if len(idxs) > 0:
+            # loop over the indexes we are keeping
+            for i in idxs.flatten():
+                # extract the bounding box coordinates
+                x, y = boxes[i][0], boxes[i][1]
+                w, h = boxes[i][2], boxes[i][3]
+                # draw a bounding box rectangle and label on the image
+
+                color = [int(c) for c in COLORS[class_ids[i]]]
+                cv2.rectangle(image, (x, y), (x + w, y + h), color=color, thickness=thickness)
+                text = LABELS[class_ids[i]]
+                # calculate text width & height to draw the transparent boxes as background of the text
+                (text_width, text_height) = cv2.getTextSize(text, cv2.FONT_HERSHEY_SIMPLEX, fontScale=font_scale, thickness=thickness)[0]
+                text_offset_x = x
+                text_offset_y = y - 5
+                box_coords = ((text_offset_x, text_offset_y), (text_offset_x + text_width + 2, text_offset_y - text_height))
+                overlay = image.copy()
+                cv2.rectangle(overlay, box_coords[0], box_coords[1], color=color, thickness=cv2.FILLED)
+                # add opacity (transparency to the box)
+                image = cv2.addWeighted(overlay, 0.6, image, 0.4, 0)
+                # now put the text (label: confidence %)
+                cv2.putText(image, text, (x, y - 5), cv2.FONT_HERSHEY_SIMPLEX,
+                            fontScale=font_scale, color=(0, 0, 0), thickness=thickness)
+
+		try:
+		    self.image_pub.publish(self.bridge.cv2_to_imgmsg(image, "bgr8"))
+		except CvBridgeError as e:
+		    print(e)
 
 def main(args):
-  rospy.init_node('image_converter', anonymous=True)
-  ic = image_converter()
-  try:
-    rospy.spin()
-  except KeyboardInterrupt:
-    print("Shutting down")
-  cv2.destroyAllWindows()
+    rospy.init_node('image_converter', anonymous=True)
+    ic = image_converter()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
 
 if __name__ == '__main__':
     main(sys.argv)
